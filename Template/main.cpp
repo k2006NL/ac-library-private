b9194f75ef8e7ab27c0d4c2e2090f6801ed7b086
k2006NL <keisuke20060115@gmail.com>
2026-01-22 16:36:37 +0900

    テンプレ

---
+++ Template/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using ll = long long; // long long を ll と書けるようにする
+using namespace atcoder; 
+
+int main() {
+    // 入出力の高速化
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    // ここにコードを書く
+    
+
+    return 0;
+}
